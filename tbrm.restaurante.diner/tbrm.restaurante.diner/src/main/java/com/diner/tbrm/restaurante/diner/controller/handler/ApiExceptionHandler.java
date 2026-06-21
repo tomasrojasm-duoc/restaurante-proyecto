afb401ade9687b2e902c631ae59c478fae7afd7a
tomasrojasm-duoc <tomas.rojasm54@gmail.com>
2026-06-21 04:38:20 -0400

    logging manual diner

---
+++ tbrm.restaurante.diner/tbrm.restaurante.diner/src/main/java/com/diner/tbrm/restaurante/diner/controller/handler/ApiExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.diner.tbrm.restaurante.diner.controller.handler;
+
+import com.diner.tbrm.restaurante.diner.dto.ExceptionDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ExceptionDto>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ) {
+        List<ExceptionDto> errors = new ArrayList<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.add(new ExceptionDto(fieldName, errorMessage));
+        });
+
+        logger.warn("Error de validación en diner. totalErrores={}", errors.size());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDto> handleException(Exception ex) {
+        ExceptionDto error = new ExceptionDto();
+
+        error.setMessage("Ocurrió un error");
+        error.setDescription(ex.getMessage());
+
+        logger.error("Error general capturado en ApiExceptionHandler de diner. Motivo={}",
+                ex.getMessage(),
+                ex);
+
+        return ResponseEntity.badRequest().body(error);
+    }
+}
---
+++ tbrm.restaurante.diner/tbrm.restaurante.diner/src/main/java/com/diner/tbrm/restaurante/diner/dto/ExceptionDto.java
@@ -0,0 +1,14 @@
+package com.diner.tbrm.restaurante.diner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionDto {
+
+    private String message;
+    private String description;
+}
